fe6a3c49c7bdd207e16f7d8d56cc5f3005a39e19
kelvin-010 <oiograpes52@gmail.com>
2025-02-26 19:52:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Kelvin Bot",
   botOwner: 'kelvin', // Your name
   ownerNumber: '260954678149', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
